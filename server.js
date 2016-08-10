22a79639aea8461318aaadc63fe5ba393821fe8b
bluedge <nico@bluedge.co.uk>
2016-08-10 11:01:25 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,18 +4,18 @@ require('babel-core/register')({
 
 
 // REQUIRES
-var express				= require("express");
-var jade				= require("jade");
-var app					= express();
+var express = require("express");
+var jade = require("jade");
+var app = express();
 
 
 // REACT (for SEO)
-var React 				= require('react');
-var ReactDOMServer 		= require('react-dom/server');
+var React = require('react');
+var ReactDOMServer = require('react-dom/server');
 
 // This is our React component
 // NOTE : we require the app.js file NOT the main.js
-var Comp				= React.createFactory(require('./src/app'));
+var Comp = React.createFactory(require('./src/app'));
 
 // HTML REACT OUTPUT
 var seo = ReactDOMServer.renderToString(Comp());
@@ -30,14 +30,14 @@ app.set("views", "./views");
 
 // Serve index file
 app.get("/", function(req, res) {
-	res.locals.seo = seo;
-	res.render('index');
+  res.locals.seo = seo;
+  res.render('index');
 });
 
 
 // Listen
 var server = app.listen(3000, 'localhost', function() {
-	var host = server.address().address;
-	var port = server.address().port;
-	console.log('Server listening http://%s:%s', host, port);
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Server listening http://%s:%s', host, port);
 });
